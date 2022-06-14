d224ecd55dff4939bfc482ba61cbba2fc273e6f9
Zachary Michels <zachary.michels@gmail.com>
2022-06-14 12:00:06 -0700

    improved sliding window definition
    The previous version of this function segmented the EBSD region into 3x3 arrays. This new version uses a sliding window.

--- gridCVA_old.m
+++ gridCVA_old.m
@@ -39,7 +39,7 @@
 
 
 %%
-function [eCVA,bv] = gridCVA(ePhase)
+function [eV,mags,bv,eId] = gridCVA(ePhase)
 %%
 
 % gridify
@@ -54,32 +54,22 @@ ids = ePhase.id;
 % window width
 w = 3;
 
-% indices/ids of center points
-row = 2:1:max(a1)-1;
-col = 2:1:max(b1)-1;
+% make sure the matrix of ids is divisible by w
+a = w*floor(a1/w);
+b = w*floor(b1/w);
+ids = ids(1:a,1:b);
 
-inds = ids(2:a1-1,2:b1-1);
-eId = inds(:);
-
-%% initialize window
-num = length(eId);
-win1 = zeros(w,w,num);
-
-for s = 1:num
-% s = 1;
-c = inds(s);
-
-win1(:,:,s) =   [c-a1-1  c-1 c+a1-1;
-                c-a1    c   c+a1;
-                c-a1+1  c+1 c+a1+1];
-
-end
-
-%% assign orientations
-oMat = ePhase(win1).orientations;
+% break up the matrix into cells w x w cells
+win1 = mat2cell(ids,[w*ones(a/w,1)'],[w*ones(b/w,1)']);
 
 
-%%
+
+% list of cells
+wins = win1(:);
+
+% number of grains:
+num=length(wins);
+
 
 % for keeping track of progress in for loop:
 div=round(num/10);
@@ -88,26 +78,21 @@ count=div;
 % pre-allocate
 eV = [vector3d(nan(3,num));vector3d(nan(3,num));vector3d(nan(3,num))];
 mags = nan(3,num);
-kos = nan(size(eId));
-kax = [vector3d(nan(3,num));vector3d(nan(3,num));vector3d(nan(3,num))];
+eId = nan(1,num);
 
 %% analysis loop
 fprintf('\n\n%i kernels\n\n',num)
 
-for n = 1:num
-    
-    o = oMat(:,:,n);
-    o = o(~isnan(o(:)));
-   
-    if length(o)>2 && max(angle(o,mean(o)))>.01*degree
+for n = 1:length(wins)
+    m = wins{n};
+    if sum(ePhase(m).phase)>0
+        o = ePhase(m).orientations;
+        o = o(~isnan(o));
         
-        [eV(:,n),mags(:,n)] = PGA(o);
-        
-        % kernel orientation spread (KOS - like mis2mean for kernel)
-        kos(n) = max(angle(o,mean(o)));
-        % kernel mean KOS axis
-         kax(n) = mean(axis(o,mean(o)));
-
+        if length(o)>=3
+            [eV(:,n),mags(:,n)] = PGA(o);
+            eId(n) = m(ceil(numel(m)/2));
+        end
     end
     
     % Keep track of for loop progress and print to consoloe screen:
@@ -121,15 +106,6 @@ end
 % project to lower hemisphere
 eV(eV.z>0)=-eV(eV.z>0);
 
-%% append ebsd variable
-eCVA = ePhase(eId);
-eCVA.prop.CVA = eV(1,:);
-eCVA.prop.eV1 = eV(1,:);
-eCVA.prop.eV2 = eV(2,:);
-eCVA.prop.eV3 = eV(3,:);
-eCVA.prop.mags = mags;
-eCVA.prop.kos = kos;
-
 %% Handle results
 % % assign 'cva' from eignevector results
 % eV1 = eV(1,:);
@@ -138,15 +114,12 @@ eCVA.prop.kos = kos;
 % mag = mags(1,:);
 
 % identify null solutions
-cond = (norm(eV(1,:))==0 | isnan(mags(1,:)) | isnan(eV(1,:)));
+cond = (norm(eV(1,:))==0 | isnan(mags(1,:)));
 
 % apply condition
-eCVA(cond) = [];
-% eV(:,cond) = [];
-% eId(cond) = [];
-% mags(:,cond) = [];
-% kos(cond) = [];
-% kax(cond) = [];
+eV(:,cond) = [];
+eId(cond) = [];
+mags(:,cond) = [];
 
 %% Kernel Density Estimation to get a best fit "bulk" vorticity vector.
 % Define a kernel density estimation with specified halfwidth. MTEX default
@@ -159,6 +132,5 @@ kde = calcDensity([eV(1,:) -eV(1,:)],r,'antipodal','halfwidth',10*degree);
 % get vector and negated vector (antipodal) of best-fit axis:
 bv=[r(I),-r(I)];
 bv(bv.z>0) = [];
-
 
 end
